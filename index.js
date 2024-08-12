73769f30d38f2dd23026f38114ad1694ac54d572
tushar singh <anshulsingh62195@gmail.com>
2024-08-13 01:25:49 +0530

    Update index.js

--- index.js
+++ index.js
@@ -3,7 +3,7 @@ const cors = require('cors')
 const app = express()
 
 app.use(express.static('dist'))
-// app.use(express.json())
+app.use(express.json())
 // app.use(requestLogger)
 
 let notes = [
